cd503a78992b768a0c7f4b9bd211de891d09e036
rkbo07 <166475438+rkbo07@users.noreply.github.com>
2024-04-08 23:54:52 -0500

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,7 @@
+
+        function clickButton(){
+            document.getElementById("demo").innerHTML = "Second Part"
+            document.getElementById("demo").style = "display:block"
+//            document.write(5 + 6)
+            console.log(5 + 6)
+        }
